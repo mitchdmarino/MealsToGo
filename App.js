bbad095eee8568e04b2a5682c98001cc3703fd57
mitchdmarino <mitchdmarino@gmail.com>
2022-08-03 17:47:27 -0700

    searchbar and folder architecture set

--- App.js
+++ App.js
@@ -1,35 +1,11 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.searchBox}>
-          <Text>Search</Text>
-        </View>
-        <View style={styles.listBox}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
-      <ExpoStatusBar style="auto" />
+      <RestaurantsScreen />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  searchBox: {
-    padding: 16,
-    backgroundColor: "yellow",
-  },
-  listBox: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "#123432",
-  },
-});
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Searchbar } from "react-native-paper";
+
+import { RestaurantInfo } from "../components/restaurant-info.component";
+
+export const RestaurantsScreen = () => {
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchBox}>
+          <Searchbar />
+        </View>
+        <View style={styles.listBox}>
+          <RestaurantInfo />
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  searchBox: {
+    padding: 16,
+    backgroundColor: "yellow",
+  },
+  listBox: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#123432",
+  },
+});
